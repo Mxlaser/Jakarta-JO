a5aa06da53fea4267883c826e3cefbdf72a14dea
niko <nikola.zivkovic300@gmail.com>
2024-04-18 09:54:45 +0200

    Creation projet

---
+++ src/main/java/java/fr/JakartaJO/JakartaJoApplication.java
@@ -0,0 +1,13 @@
+package java.fr.JakartaJO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakartaJoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JakartaJoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/fr/JakartaJO/JakartaJoApplicationTests.java
@@ -0,0 +1,13 @@
+package java.fr.JakartaJO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JakartaJoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
