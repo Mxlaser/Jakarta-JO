8511b4067178f0d5e1b61dcfef0ce1a947176d3e
niko <nikola.zivkovic300@gmail.com>
2024-04-18 11:37:39 +0200

    Epreuve 2

--- src/main/java/fr/efrei/jakartajo/service/EpreuveService.java
+++ src/main/java/fr/efrei/jakartajo/service/EpreuveService.java
@@ -1,9 +1,14 @@
 package fr.efrei.jakartajo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import fr.efrei.jakartajo.model.Epreuve;
 import fr.efrei.jakartajo.model.Utilisateur;
+import fr.efrei.jakartajo.repository.EpreuveRepository;
+import jakarta.transaction.Transactional;
 
 @Service
 public class EpreuveService {
@@ -14,5 +19,26 @@ public class EpreuveService {
     public EpreuveService(EpreuveRepository repository, UtilisateurService utilisateurService) {
         this.repository = repository;
         this.utilisateurService = utilisateurService;
+    }
+
+    public List<Epreuve> findAll() {
+        return repository.findAll();
+    }
+
+    public Epreuve save(Epreuve epreuve) {
+        return repository.save(epreuve);
+    }
+
+    @Transactional
+    public Epreuve ajoutEtudiant(String epreuveId, String utilisateurId) {
+        Epreuve epreuve = repository.findById(epreuveId).orElse(null);
+        Utilisateur utilisateur = utilisateurService.findUtilisateurById(utilisateurId);
+        epreuve.getUtilisateurs().add(utilisateur);
+        repository.save(epreuve);
+        return epreuve;
+    }
+
+    public void delete(String id) {
+        repository.deleteById(id);
     }
 }
