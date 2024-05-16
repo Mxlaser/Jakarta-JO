cd55ec89aacb5a206248cf6f2f29970986b09112
NicolasBELLANGER <nicobell93220@gmail.com>
2024-05-16 11:20:03 +0200

    Creation Billet

---
+++ src/main/java/fr/efrei/jakartajo/model/Billet.java
@@ -0,0 +1,62 @@
+package fr.efrei.jakartajo.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Billet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String uuid;
+    private String name;
+    private Float price;
+    private LocalDateTime createdAt = null;
+    private LocalDateTime deletedAt = null;
+
+    public Billet(){
+
+    }
+
+    public Billet(String name, Float price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(LocalDateTime deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+}
