4bdfad50d9f94621f61696a17c23a5477c88e10f
niko <nikola.zivkovic300@gmail.com>
2024-05-16 09:29:48 +0200

    AcheteurService V2

--- src/main/java/fr/efrei/jakartajo/service/AcheteurService.java
+++ src/main/java/fr/efrei/jakartajo/service/AcheteurService.java
@@ -1,5 +1,6 @@
 package fr.efrei.jakartajo.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import fr.efrei.jakartajo.model.Acheteur;
 import fr.efrei.jakartajo.repository.AcheteurRepository;
+import jakarta.transaction.Transactional;
 
 @Service
 public class AcheteurService {
@@ -24,5 +26,20 @@ public class AcheteurService {
 
     public Acheteur findAcheteurById(String uuid) {
         return repository.findOneByUuid(uuid).orElse(null);
+    }
+
+    public Acheteur create(Acheteur acheteur) {
+        return repository.save(acheteur);
+    }
+
+    @Transactional
+    public boolean delete(String uuid) {
+        Acheteur acheteurASupprimer = findAcheteursById(uuid);
+        if (acheteurASupprimer != null && acheteurASupprimer.getDeletedAt() == null) {
+            acheteurASupprimer.setDeletedAt(LocalDateTime.now());
+            repository.save(acheteurASupprimer);
+            return true;
+        }
+        return false;
     }
 }
