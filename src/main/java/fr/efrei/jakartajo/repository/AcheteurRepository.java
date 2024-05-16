3eaf013eccdc3c582b047bbc6c5ea8b88e0ec327
niko <nikola.zivkovic300@gmail.com>
2024-05-16 09:16:24 +0200

    AcheteurRepository

--- src/main/java/fr/efrei/jakartajo/repository/AcheteurRepository.java
+++ src/main/java/fr/efrei/jakartajo/repository/AcheteurRepository.java
@@ -1,5 +1,16 @@
 package fr.efrei.jakartajo.repository;
 
-public class AcheteurRepository {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import fr.efrei.jakartajo.model.Acheteur;
+
+@Repository
+public interface AcheteurRepository extends JpaRepository<Acheteur, String> {
+    List<Acheteur> findAll();
+
+    Optional<Acheteur> findOneByUuid(String uuid);
 }
