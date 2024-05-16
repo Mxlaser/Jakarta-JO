7c28f43b9de6672d045614a7302c3de35747bdb4
niko <nikola.zivkovic300@gmail.com>
2024-05-16 08:54:05 +0200

    AcheteurService

--- src/main/java/fr/efrei/jakartajo/service/AcheteurService.java
+++ src/main/java/fr/efrei/jakartajo/service/AcheteurService.java
@@ -1,5 +1,28 @@
 package fr.efrei.jakartajo.service;
 
-public class AcheteurService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fr.efrei.jakartajo.model.Acheteur;
+import fr.efrei.jakartajo.repository.AcheteurRepository;
+
+@Service
+public class AcheteurService {
+    private final AcheteurRepository repository;
+
+    @Autowired
+    public AcheteurService(AcheteurRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Acheteur> findAcheteurs() {
+        System.out.println("Trouvé");
+        return repository.findAll();
+    }
+
+    public Acheteur findAcheteurById(string uuid) {
+        return repository.findOneByUuid(uuid).orElse(null);
+    }
 }
