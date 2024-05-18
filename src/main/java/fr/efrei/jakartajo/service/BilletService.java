a9bc0c1d71f7121be18e8a3e5863ceecd2d2d6e8
NicolasBELLANGER <nicobell93220@gmail.com>
2024-05-18 10:06:08 +0200

    push

--- src/main/java/fr/efrei/jakartajo/service/BilletService.java
+++ src/main/java/fr/efrei/jakartajo/service/BilletService.java
@@ -1,5 +1,14 @@
 package fr.efrei.jakartajo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fr.efrei.jakartajo.repository.BilletRepository;
+
+@Service
 public class BilletService {
+    private final BilletRepository repository;
+
+    @Autowired
 
 }
